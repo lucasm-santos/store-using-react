b9187ceb9db9435c436d79266d5122fc6bdd78a1
lucas-msantos <lucasmsantos.7407@gmail.com>
2025-01-22 20:17:09 -0300

    mudança de estados dos campos de registro e logica da senha no register

--- src/Components/LoginRegister/LoginRegister.jsx
+++ src/Components/LoginRegister/LoginRegister.jsx
@@ -7,6 +7,16 @@ const LoginRegister = () => {
 
     const [action, setAction] = useState('');
 
+    const [userName, setUserName] = useState('');
+
+    const [password, setPassword] = useState('');
+
+    const [confPassword, setConfPassword] = useState('');
+
+    const [submited, setSubmited] = useState(false);
+
+    const passwordValid = password == confPassword && password.length >= 10;
+
     const registerLink = () => {
         setAction(' active ')
     };
@@ -18,12 +28,16 @@ const LoginRegister = () => {
     return (
         <div className={`wrapper${action}`}>
             <div className="form-box login">
-                <form action="">
+                <form onSubmit={(event) => {
+                    event.preventDefault()
+                    setSubmited(true)
+                }} action="">
                     <h1>Login</h1>
                     <label className="input-box">
-                        <input type="text" placeholder="Username" required />
+                        <input type="text" placeholder="Username" onChange={(item) => setUserName(item.currentTarget.value)} value={userName} />
                         <FaUser className="icon" />
                     </label>
+                    <p>{submited && userName.length < 5 && 'ajeita ae'}</p>
                     <div className="input-box">
                         <input type="password" placeholder="Password" required />
                         <FaLock className="icon" />
@@ -50,19 +64,20 @@ const LoginRegister = () => {
                         <IoMdMail className="icon" />
                     </div>
                     <div className="input-box">
-                        <input type="password" placeholder="Password" required />
+                        <input type="password" placeholder="Password" required minLength={10} maxLength={25} onChange={(event) => setPassword(event.currentTarget.value)} />
                         <FaLock className="icon" />
                     </div>
                     <div className="input-box">
-                        <input type="password" placeholder="Confirm Password" required />
+                        <input type="password" placeholder="Confirm Password" minLength={10} maxLength={25} onChange={(event) => setConfPassword(event.currentTarget.value)} />
                         <FaLock className="icon" />
                     </div>
+                    {password != confPassword && 'ta igual nn prc'}
                     <div className="remember-forgot">
                         <label><input type="checkbox" required />I agree to the terms & conditions</label>
                     </div>
-                    <button type="submit">Register</button>
+                    <button type="submit" disabled={!passwordValid}>Register</button>
                     <div className="register-link">
-                        <p>Already have an account? <a href="#" onClick={loginLink}>Sign</a></p>
+                        <p>Already have an account? <a href="#" onClick={loginLink}>Sign up</a></p>
                     </div>
                 </form>
             </div>
